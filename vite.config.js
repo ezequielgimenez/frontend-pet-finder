846ffd252495b9063ff789f0d95900c30e819718
Ezequiel <ezequielezequiel9@gmail.com>
2024-08-01 13:48:59 -0300

    edit vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,25 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      components: path.resolve(__dirname, "src/components"),
-      pages: path.resolve(__dirname, "src/pages"),
-      router: path.resolve(__dirname, "src/router"),
-      ui: path.resolve(__dirname, "src/ui"),
-      hooks: path.resolve(__dirname, "src/hooks"),
-      lib: path.resolve(__dirname, "src/lib"),
+export default defineConfig(({ mode }) => {
+  // Cargar variables de entorno según el modo
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        components: path.resolve(__dirname, "src/components"),
+        pages: path.resolve(__dirname, "src/pages"),
+        router: path.resolve(__dirname, "src/router"),
+        ui: path.resolve(__dirname, "src/ui"),
+        hooks: path.resolve(__dirname, "src/hooks"),
+        lib: path.resolve(__dirname, "src/lib"),
+      },
     },
-  },
+    define: {
+      "process.env": env,
+    },
+  };
 });
